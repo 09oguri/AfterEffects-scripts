0a4f5d760438cf769d72df6b7745bea0753398f9
09oguri <oguri.h.aa@m.titech.ac.jp>
2014-09-12 00:11:58 +0900

    add scripts
    AddTextLayers
    SetCompTimeAll

---
+++ AddTextLayers.jsx
@@ -0,0 +1,75 @@
+﻿// 次の行のコメントを解除するとダブルクリックでスクリプトが実行できる
+// #target aftereffects
+
+
+// #### 設定 ####
+// テキスト表示位置
+var X = 1280 * 0.5;
+var Y = 720 * 0.9;
+
+
+// #### 処理 ####
+var text = [];
+
+main();
+
+function main() {
+	// アクティブなアイテムを取得
+	var activeComp = app.project.activeItem;
+
+	// 取得したアイテムがコンポジションでない場合は終了
+	if ((activeComp == null) || !(activeComp instanceof CompItem)) {
+		alert("テキストを作成したいコンポジションをアクティブにして下さい");
+		return;
+	}
+
+	var isLoaded = loadFile();
+	if (isLoaded < 0) {
+		return;
+	}
+
+	app.beginUndoGroup("script: AddTextLayers");
+
+	// テキストレイヤーを追加
+	for (var i = 0; i < text.length; i++) {
+		activeComp.layers.addText(text[i]);
+		activeComp.selectedLayers[0].position.setValue([X, Y]);
+	}
+
+	app.endUndoGroup();
+}
+
+//テキストファイルを行毎に読み込む
+function loadFile() {
+	try {
+		var filename = File.openDialog("テキストファイルを指定してください");
+		if (!filename) {
+			throw new Error("ファイルが指定されていません");
+		}
+
+		var fileObj = new File(filename);
+		var isOpened = fileObj.open("r");
+		if (!isOpened) {
+			throw new Error("ファイルを開けませんでした");
+		}
+
+		var count = 0;
+		while (!fileObj.eof) {
+			line = fileObj.readln();
+
+			// 空行を無視
+			if (line == "") {
+				continue;
+			}
+
+			text.push(line);
+			count++;
+		}
+		fileObj.close();
+
+		return count;
+	} catch(e) {
+		alert(e);
+		return -1;
+	}
+}
---
+++ SetCompTimeAll.jsx
@@ -0,0 +1,58 @@
+﻿// 次の行のコメントを解除するとダブルクリックでスクリプトが実行できる
+// #target aftereffects
+
+
+// #### 設定 ####
+FPS = 24
+
+
+// #### 処理 ####
+main();
+
+function main() {
+	var inputWindow = new Window("dialog","コンポの時間を統一",[0, 0, 180, 80]);
+	inputWindow.center();
+
+	inputWindow.add("statictext", [0, 0, 100, 20], "コンポジションの時間");
+	var inputSec = inputWindow.add("edittext", [0, 25, 50, 45], "0");
+	inputWindow.add("statictext", [55, 25, 80, 45], "秒 + ");
+	var inputFrame = inputWindow.add("edittext", [85, 25, 135, 45], "0");
+	inputWindow.add("statictext", [140, 25, 180, 45], "フレーム");
+
+	var buttonOK = inputWindow.add('button', [70, 50, 120, 70], 'OK' );
+	var buttonCancel = inputWindow.add('button', [ 125,  50,  175,  70], 'Cancel' );
+
+	// キャンセルされた場合は終了
+	if (inputWindow.show() != 1) {
+		return;
+	}
+
+	// 入力からコンポの時間を計算
+	var duration = 0;
+	if (isNumber(inputSec.text) && isNumber(inputFrame.text)) {
+		duration = parseInt(inputSec.text) + parseInt(inputFrame.text) / FPS;
+	} else {
+		alert("入力が不正です");
+		return;
+	}
+
+	app.beginUndoGroup("script: SetCompTimeAll");
+
+	// コンポの時間を変更
+	for (var i=1; i < app.project.items.length + 1; i++) {
+		var item = app.project.item(i);
+		if ((item != null) && item instanceof CompItem) {
+			item.duration = duration;
+		}
+	}
+
+	app.endUndoGroup();
+}
+
+// 入力をチェック
+function isNumber(time) {
+	if (time && !isNaN(parseInt(time))) {
+		return true;
+	}
+	return false;
+}
